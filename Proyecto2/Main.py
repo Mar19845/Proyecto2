979d8a4e106a73ffbbfe582587c39b8fbe2f3b50
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2020-05-30 21:19:58 -0600

    Update Main.py

--- Proyecto2/Main.py
+++ Proyecto2/Main.py
@@ -2,12 +2,39 @@ from ClaseBaseDeDatosMusica import BaseMusica
 from Grafo import grafos
 from neo4j import GraphDatabase
 from User import Usuario
+
+def menu2():
+    print("Bienvenido!")
+    print("\n1. Eliminar Datos de la base de Datos\n2. Ver Las Canciones Disponible\n3. Recomendar canciones similares a una Cancion que uste ponga?\n4. Mostrar Grafo de relaciones")
+    cat1 = input("Eleccion a realizar: ")
+    if cat1=='1':
+        print("")
+        print("2")
+    elif cat1=='2':
+        print("")
+        print("pp")
+    elif cat1=='3':
+        print("")
+        cancion = (input("Ingrese la cancion: "))
+        n = BaseMusica()
+        print(n.RecomendarCancion(cancion))
+    elif cat1=='4':
+        print("")
+        #from Grafo import grafos
+        s = grafos()
+        print(s.Diagrama())
+    else:
+        print("")
+        print("No reconozco tu eleccion!")
+
+
+
 print('''Bienvenido al Sistema que recomienda Musica''')
 print("")
-print('''Los Creadores Juan Marroquin Y Carlos Raxtum le damos la bienvenida
+print('''Los Creadores Juan Marroquin y Carlos Raxtum le damos la bienvenida
 Esperamos le guste el sistema de recomendacion
 
-A continuacion el menu''')
+Eliga una accion!''')
 
 cat=''
 n1 = Usuario()
@@ -15,45 +42,27 @@ while cat!='Salir':
     cat=input('''
 1. Crear Usuario
 2. Login
-3. Eliminar Datos de la base de Datos
-4. Ver Las Canciones Disponible
-5. Recomendar canciones similares a una Cancion que uste ponga?
-6. Mostrar Grafo de relaciones
-7. Fin de programa
+3. Fin de programa
 
 Ingrese la funcion que desea usar:  ''')
-    if cat=='7':
+    if cat=='3':
         print("")
         print("Gracias por hacer uso del sistema que recomineda Musica")
         break
-    if cat=='1':
+    elif cat=='1':
         id= (input("Ingrese el nombre de Usuario que quiere usar: "))
         pas = (input("Ingrese su nuevo password: "))
         n1.CrearUser(id,pas)
-    if cat=='2':
+    elif cat=='2':
         id= (input("Ingrese el nombre de Usuario: "))
         pas = (input("Ingrese su nuevo password: "))
         n1.login(id,pas)
         if(n1.boolean == True):
-            print("PPPPP")
-    if cat=='3':
-        print("")
-        print("3")
-    if cat=='4':
-        print("")
-        print("pp")
-    if cat=='5':
-        print("")
-        cancion = (input("Ingrese la cancion: "))
-        n = BaseMusica()
-        print(n.RecomendarCancion(cancion))
-    if cat=='6':
-        print("")
-        #from Grafo import grafos
-        s = grafos()
-        print(s.Diagrama())
+            menu2()
     else:
         print("")
-        print("Porvafor seleccionar una opcion valida")
+        print("No reconozco tu eleccion!")
+        
+        
         
         
